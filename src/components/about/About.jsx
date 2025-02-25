be1c75dfd19093960a6e7da6a35ecfd498450d25
saanvishukla <sshukla@connect.ust.hk>
2025-02-25 23:08:52 +0800

    forth

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -54,36 +54,36 @@ const About = () => {
           className="boxAbout"
           whileHover={{ background: "lightgray", color: "black" }}
         >
-          <h2>Education</h2>
-          <p>
-          I'm currently pursuing my degree in Computer Science and Engineering at HKUST. This journey has been incredibly enriching, allowing me to delve into subjects like Python, C++, AI, and statistics. I've also had the opportunity to apply these skills in real-world projects and internships, which has been truly valuable.
+          <h2 className="head">Education</h2>
+          <p >
+          I'm pursuing a degree in Computer Science and Engineering at HKUST. This journey has been enriching, allowing me to explore subjects like Python, C++, AI, and statistics. I've applied these skills in real-world projects and internships, gaining valuable experience. The program provides a solid foundation in computer science, preparing me for a career in tech. Through projects and internships, I've gained practical skills that complement my theoretical knowledge.
           </p>
         </motion.div>
         <motion.div
           className="boxAbout"
           whileHover={{ background: "lightgray", color: "black" }}
         >
-          <h2>Skills</h2>
-          <p>
-          I've developed a strong set of skills that blend technical expertise with creative problem-solving. I'm proficient in languages like Python, C++, and SQL, which have been invaluable in my work with AI and data analysis. Tools like Pandas, NumPy, and Scikit-learn have allowed me to dive deep into data science projects, while platforms like Google Cloud and Firebase have helped me build scalable applications. Beyond coding, I've honed my skills in communication and teamwork, which are essential for collaborating effectively in multi-disciplinary environments.
+          <h2 className="head">Skills</h2>
+          <p >
+          I've developed strong technical skills in languages like Python, C++, and SQL. Tools like Pandas and NumPy have been invaluable in data science projects. I've also honed my communication and teamwork skills, essential for collaborating effectively. My technical expertise is complemented by creative problem-solving abilities, allowing me to contribute to various projects. I'm proficient in building scalable applications using platforms like Google Cloud and Firebase.
           </p>
         </motion.div>
         <motion.div
           className="boxAbout"
           whileHover={{ background: "lightgray", color: "black" }}
         >
-          <h2>Co Curriculars</h2>
-          <p>
-          I'm passionate about extracurricular activities that complement my studies. As Vice President of WISE at HKUST, I've led initiatives that boosted participation by 80%. I'm also an Engineering Student Ambassador, promoting the engineering program and enhancing its visibility. Additionally, my work with the Investment Circle has allowed me to apply my analytical skills using Python, contributing to a 15% improvement in portfolio performance
+          <h2 className="head">Co Curriculars</h2>
+          <p >
+          I'm passionate about extracurricular activities that complement my studies. As Vice President of WISE at HKUST, I boosted participation by 80%. I'm also an Engineering Student Ambassador, promoting the program and enhancing its visibility. My work with the Investment Circle improved portfolio performance by 15% using Python. These roles have helped me develop leadership and teamwork skills, essential for professional success.
           </p>
         </motion.div>
         <motion.div
           className="boxAbout"
           whileHover={{ background: "lightgray", color: "black" }}
         >
-          <h2>Work Experience</h2>
-          <p>
-          I've had a diverse range of professional experiences that have shaped my skills and perspectives. Currently, I'm working as an Information Technology Intern at Tata Steel, where I've developed a website with a Python-based PDF extraction system, enhancing operational efficiency for key clients like Indian Railways. I'm also a Software Developer at Vision Drop AI, contributing to the development of NokkenTalk AI, which will be showcased at CES 2025. Additionally, I've interned at MSTC Limited, optimizing database architecture and automating ETL processes, and at Wallem Shipmanagement Limited, where I streamlined workflows by integrating systems with Microsoft D365.
+          <h2 className="head">Work</h2>
+          <p >
+         I've had diverse professional experiences that have shaped my skills. Currently, I'm an IT Intern at Tata Steel, developing a Python-based PDF extraction system. I'm also a Software Developer at Vision Drop AI, contributing to NokkenTalk AI. Additionally, I've interned at MSTC Limited and Wallem Shipmanagement Limited, optimizing database architecture and streamlining workflows. These experiences have provided valuable insights into industry practices and technologies.
           </p>
         </motion.div>
       </motion.div>
